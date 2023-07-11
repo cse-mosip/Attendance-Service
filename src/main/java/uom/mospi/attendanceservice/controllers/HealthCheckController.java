2a76f471b9ddde776fd1de65a829edfa83faf6bf
Dinuka <sec117tion@gmail.com>
2023-07-11 13:23:16 +0530

    Added healthCheck endpoint

---
+++ src/main/java/uom/mospi/attendanceservice/controllers/HealthCheckController.java
@@ -0,0 +1,13 @@
+package uom.mospi.attendanceservice.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/healthcheck")
+    public String healthCheck() {
+        return "Ok";
+    }
+}
