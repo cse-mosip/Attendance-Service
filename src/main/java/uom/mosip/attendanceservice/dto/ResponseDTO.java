24ae24c6bdc17ac9a2f2a97a1b326d68e8af66fd
Dinuka <sec117tion@gmail.com>
2023-07-11 18:04:58 +0530

    Added a default Response DTO

---
+++ src/main/java/uom/mosip/attendanceservice/dto/ResponseDTO.java
@@ -0,0 +1,18 @@
+package uom.mosip.attendanceservice.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ResponseDTO {
+    private String status;
+    private String message;
+    private Object data;
+}
