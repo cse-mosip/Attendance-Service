bdc27f980ceaaa838ab98558c2bae1df043295a9
Dinuka <sec117tion@gmail.com>
2023-07-13 00:21:30 +0530

    Added Authentication Filter

--- src/main/java/uom/mosip/attendanceservice/controllers/AuthController.java
+++ src/main/java/uom/mosip/attendanceservice/controllers/AuthController.java
@@ -2,6 +2,7 @@ package uom.mosip.attendanceservice.controllers;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +18,12 @@ public class AuthController {
     @PostMapping("/admin/login")
     public Object login(@Valid @RequestBody LoginRequestDTO loginRequestDTO) {
 
-        //return tokenService.generateJWTToken(loginRequestDTO.getUsername(), "1");
-        return tokenService.decodeJWTToken(loginRequestDTO.getUsername());
+        return tokenService.generateJWTToken(loginRequestDTO.getUsername(), "1");
+        // return tokenService.decodeJWTToken(loginRequestDTO.getUsername());
+    }
+
+    @GetMapping("/pend")
+    public String pend() {
+        return "This is a protected resource.";
     }
 }
