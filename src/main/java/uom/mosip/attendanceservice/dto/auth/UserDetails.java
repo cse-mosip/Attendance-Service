ce29e2589682aa943be46f960a90a670d7965e97
Kabilan Mahathevan <kabilan_m@thesoftwarepractice.com>
2023-07-13 17:20:11 +0530

    Merge conflict resolved

---
+++ src/main/java/uom/mosip/attendanceservice/dto/auth/UserDetails.java
@@ -0,0 +1,15 @@
+package uom.mosip.attendanceservice.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDetails {
+    private long userID;
+    private int userType;
+}
