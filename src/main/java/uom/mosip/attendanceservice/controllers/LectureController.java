e91d58a1f2a2c155df7ac273cec703681c86a8bf
Sathsarani-Kapukotuwa <sathsaranikapukotuwa@gmail.com>
2023-07-16 21:32:30 +0530

    create lecture added

--- src/main/java/uom/mosip/attendanceservice/controllers/LectureController.java
+++ src/main/java/uom/mosip/attendanceservice/controllers/LectureController.java
@@ -3,11 +3,9 @@ package uom.mosip.attendanceservice.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import uom.mosip.attendanceservice.dto.LectureDTO;
 import uom.mosip.attendanceservice.dto.ResponseDTO;
 import uom.mosip.attendanceservice.services.LectureService;
 
@@ -41,6 +39,11 @@ public class LectureController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseDTO);
         }
         return ResponseEntity.status(HttpStatus.OK).body(responseDTO);
+    }
+
+    @PostMapping(path="/createLecture", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ResponseDTO> createLecture(@RequestBody LectureDTO lectureDTO){
+        return new ResponseEntity<>(lectureService.createLecture(lectureDTO), HttpStatus.OK);
     }
 
     @GetMapping(path = "/getAllLectures", produces = MediaType.APPLICATION_JSON_VALUE)
