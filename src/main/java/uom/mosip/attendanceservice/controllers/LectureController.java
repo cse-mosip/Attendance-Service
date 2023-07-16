dbc7f7b0ed92e97c86d96e3cfd6e89ae160454d8
Sathira Liyanapathirana <liyanapathiranasathira@gmail.com>
2023-07-16 17:13:49 +0530

    Infinite Exception corrected

--- src/main/java/uom/mosip/attendanceservice/controllers/LectureController.java
+++ src/main/java/uom/mosip/attendanceservice/controllers/LectureController.java
@@ -9,29 +9,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uom.mosip.attendanceservice.dto.ResponseDTO;
-import uom.mosip.attendanceservice.models.Lecture;
 import uom.mosip.attendanceservice.services.LectureService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("admin/lecture")
 @CrossOrigin
 public class LectureController {
     private final LectureService lectureService;
-    private static final Logger logger = LoggerFactory.getLogger(LectureController.class);
 
     @Autowired
     public LectureController(LectureService lectureService) {
         this.lectureService = lectureService;
     }
 
-    @GetMapping(path = "/getAllLectures")
+    @GetMapping(path = "/getAllLectures", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ResponseDTO> getAllLectures(){
         ResponseDTO responseDTO = lectureService.getAllLectures();
-        logger.info(responseDTO.toString());
         return ResponseEntity.status(HttpStatus.OK).body(responseDTO);
     }
 }
--- src/main/java/uom/mosip/attendanceservice/services/LectureService.java
+++ src/main/java/uom/mosip/attendanceservice/services/LectureService.java
@@ -1,10 +1,7 @@
 package uom.mosip.attendanceservice.services;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import uom.mosip.attendanceservice.controllers.LectureController;
 import uom.mosip.attendanceservice.dao.LectureRepository;
 import uom.mosip.attendanceservice.dto.ResponseDTO;
 import uom.mosip.attendanceservice.models.Lecture;
@@ -16,14 +13,12 @@ public class LectureService {
 
     @Autowired
     private LectureRepository lectureRepository;
-    private static final Logger logger = LoggerFactory.getLogger(LectureService.class);
 
     //Create lecture
 
     //Get all lectures
     public ResponseDTO getAllLectures() {
         List<Lecture> lectureList = (List<Lecture>) lectureRepository.findAll();
-        logger.info(lectureList.toString());
         return new ResponseDTO("OK", "All lectures fetched successfully", lectureList);
     }
 
