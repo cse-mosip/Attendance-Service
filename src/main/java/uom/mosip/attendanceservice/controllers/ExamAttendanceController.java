93d8b217371a7f80dacc7b9b54bbbe1bf3fd0e55
Dinuka <sec117tion@gmail.com>
2023-07-16 20:43:33 +0530

    Changed the controller of /admin/exam-attendance/${examId} endpoint

--- src/main/java/uom/mosip/attendanceservice/controllers/ExamAttendanceController.java
+++ src/main/java/uom/mosip/attendanceservice/controllers/ExamAttendanceController.java
@@ -3,19 +3,27 @@ package uom.mosip.attendanceservice.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import uom.mosip.attendanceservice.dto.ExamAttendanceDTO;
 import uom.mosip.attendanceservice.dto.MarkAttendanceRequestDTO;
 import uom.mosip.attendanceservice.dto.ResponseDTO;
+import uom.mosip.attendanceservice.models.Exam;
+import uom.mosip.attendanceservice.models.ExamAttendance;
 import uom.mosip.attendanceservice.services.ExamAttendanceService;
+import uom.mosip.attendanceservice.services.ExamService;
+import uom.mosip.attendanceservice.services.LMSService;
 
-import java.util.Objects;
+import java.util.*;
 
 @RestController
-@RequestMapping(path = "/student/exam-attendance")
 public class ExamAttendanceController {
+
+    @Autowired
+    private ExamService examService;
+
+    @Autowired
+    private LMSService lmsService;
+
     private final ExamAttendanceService examAttendanceService;
 
     @Autowired
@@ -23,7 +31,7 @@ public class ExamAttendanceController {
         this.examAttendanceService = examAttendanceService;
     }
 
-    @PostMapping(path = "/mark-exam-attendance")
+    @PostMapping(path = "/student/exam-attendance/mark-exam-attendance")
     public ResponseEntity<ResponseDTO> markExamAttendance(@RequestBody MarkAttendanceRequestDTO markAttendanceRequestDTO) {
         ResponseDTO responseDTO = examAttendanceService.markExamAttendance(markAttendanceRequestDTO);
 
@@ -31,5 +39,35 @@ public class ExamAttendanceController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseDTO);
         }
         return ResponseEntity.status(HttpStatus.OK).body(responseDTO);
+    }
+
+    @GetMapping("/admin/exam-attendance/{examId}")
+    public Object getAttendanceForAnExam(@PathVariable long examId) {
+        Optional<Exam> examOptional = examService.getAttendanceForAnExamById(examId);
+
+        if (examOptional.isEmpty())
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new ResponseDTO("EXAM_NOT_FOUND", "Exam ID is not found."));
+
+        // Get Attendance
+        Exam exam = examOptional.get();
+        List<ExamAttendance> examAttendance = exam.getAttendees();
+        Map<String, ExamAttendance> attendanceMap = new HashMap<>();
+        for (ExamAttendance ea: examAttendance) {
+            attendanceMap.put(ea.getStudentId(), ea);
+        }
+
+        // Get all enrolled Students and map their attendance
+        List<String> enrolledStudentIds = lmsService.getStudentsForACourse(exam.getModuleCode(), exam.getIntake());
+        List<ExamAttendanceDTO> attendanceDTOS = new LinkedList<>();
+        for (String studentId: enrolledStudentIds) {
+            ExamAttendance ea = null;
+            if (attendanceMap.containsKey(studentId)) {
+                ea = attendanceMap.get(studentId);
+            }
+            attendanceDTOS.add(new ExamAttendanceDTO(studentId, ea));
+        }
+
+        return new ResponseDTO("OK", "Attendance Fetched.", attendanceDTOS);
     }
 }
