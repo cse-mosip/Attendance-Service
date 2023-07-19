bc47ba50f2959092dd3fe974c7d0eb198c364466
KalinduGandara <kalindugandara16@gmail.com>
2023-07-19 13:33:22 +0530

    refactor getAttendanceForLecture

--- src/main/java/uom/mosip/attendanceservice/services/AdminService.java
+++ src/main/java/uom/mosip/attendanceservice/services/AdminService.java
@@ -3,13 +3,9 @@ package uom.mosip.attendanceservice.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uom.mosip.attendanceservice.dao.LectureAttendanceRepository;
-import uom.mosip.attendanceservice.dao.LectureRepository;
-import uom.mosip.attendanceservice.dao.lms.CourseRepository;
-import uom.mosip.attendanceservice.models.Lecture;
 import uom.mosip.attendanceservice.models.LectureAttendance;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class AdminService {
