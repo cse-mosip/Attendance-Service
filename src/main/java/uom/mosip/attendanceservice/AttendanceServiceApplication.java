8452dfe3f23dfe132b85896b3320f527dd40fa2c
section117 <sec117tion@gmail.com>
2023-08-16 12:21:20 +0530

    Set TimeZone to GMT +05.30

--- src/main/java/uom/mosip/attendanceservice/AttendanceServiceApplication.java
+++ src/main/java/uom/mosip/attendanceservice/AttendanceServiceApplication.java
@@ -1,12 +1,20 @@
 package uom.mosip.attendanceservice;
 
+import jakarta.annotation.PostConstruct;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class AttendanceServiceApplication {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("GMT+5:30"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AttendanceServiceApplication.class, args);
